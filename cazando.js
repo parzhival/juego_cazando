8d9fe69a045ac49bb21f42c090a7941b50ced1d3
parzhival <jean08070225@gmail.com>
2026-04-09 21:39:48 -0500

    feat: Variables and constants were added for positioning

--- cazando.js
+++ cazando.js
@@ -1,6 +1,30 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+const ALTO_GATO = 40;
+const ANCHO_GATO = 60;
+const ALTO_COMIDA = 40;
+const ANCHO_COMIDA = 40;
+
 function graficarGato(){
+    gatoX = canvas.height -280; 
+    gatoY = canvas.width - 270 ;
     ctx.fillStyle = "#F2EEED"
-    ctx.fillRect(canvas.width/4,canvas.height/4,canvas.width*2/4,canvas.height*2/4);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+ function graficarComida(){
+    comidaX = canvas.height-20;
+    comidaY = canvas.width - 20;  
+    ctx.fillStyle = "#04f728"
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+}
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
 }
